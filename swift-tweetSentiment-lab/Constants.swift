4b0b19ce9012cb3f2aa4c42e7b86993a1263644e
susanlovaglio <susan@flatironschool.com>
2016-07-26 16:30:04 -0400

    added constants file

---
+++ swift-tweetSentiment-lab/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  swift-tweetSentiment-lab
+//
+//  Created by Flatiron School on 7/25/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    
+    static let TWITTER_KEY = "GOOcJSI2VC9DajNOvM5oI8oXx"
+    static let SENTIMENT140_BASE_URL = "http://www.sentiment140.com/api/classify?appid=joe@flatironschool.com&language=en"
+    static let TWITTER_SECRET = "nrczlQ0yJmwEDUqEsBta6yX8akhJARglJXTPJe6UuiaC1uWaCq"
+    
+}
